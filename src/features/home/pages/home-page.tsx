a2e4a4bf4575462dd8460d88badf504883c264d5
Júlia Galhardi <juliagalhardi.c@gmail.com>
2025-05-11 18:04:36 -0300

    feat: add accordion structure

--- src/features/home/pages/home-page.tsx
+++ src/features/home/pages/home-page.tsx
@@ -4,6 +4,7 @@ import { Layout } from '@/shared/components/layout'
 import { motion } from 'framer-motion'
 import { Button } from '@/shared/components/button'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+import { Accordion, AccordionItem } from '@/shared/components/accordion'
 
 function BackgroundBlobs() {
   return (
@@ -162,6 +163,17 @@ export function HomePage() {
           <Card></Card>
           <Card></Card>
           */}
+        </section>
+        <section
+          id="faq"
+          className="flex h-screen flex-col items-center justify-center"
+        >
+          <h1 className="mb-12 text-6xl font-semibold">FAQ</h1>
+          <Accordion>
+            <AccordionItem title="Pergunta 1">Resposta 1</AccordionItem>
+            <AccordionItem title="Pergunta 2">Resposta 2</AccordionItem>
+            <AccordionItem title="Pergunta 3">Resposta 3</AccordionItem>
+          </Accordion>
         </section>
       </Layout>
     </Background>
